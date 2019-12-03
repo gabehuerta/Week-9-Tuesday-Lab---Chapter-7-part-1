42019a517c49428d61630b6f9257d7adbfba03f2
gabehuerta <54081519+gabehuerta@users.noreply.github.com>
2019-12-03 00:41:47 -0600

    Add files via upload

---
+++ CountFams.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class CountFams
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter size of family: ");
+        int k = keyboard.nextInt();
+        
+        double[] incomes = new double[k];
+        
+        double maxIncome =0;
+        for (int i = 0; i <incomes.length ; i++) {
+            System.out.print("enter income"+(i+1)+": ");
+            incomes[i] = keyboard.nextDouble();
+            if(i==0)
+            {
+                maxIncome = incomes[i];
+            }
+            if(maxIncome<incomes[i])
+            {
+                maxIncome = incomes[i];
+            }
+        }
+        
+        double value = maxIncome*0.1;
+        int count=0;
+        for (int i = 0; i < incomes.length; i++) {
+            if(incomes[i]<value)
+                count++;
+        }
+        System.out.println(count+ " families that make less than "+value+" of this "+maxIncome);
+        System.out.print("Families income: \n");
+        for (int i = 0; i < incomes.length; i++) {
+            System.out.print(incomes[i]+" ");
+        }
+        System.out.print("\n");
+    }
+}
---
+++ Initializers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Initializers
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+	int[] array01 = {2,4,6,8,10,12,14,16,18,20};
+	for (int i = 0; i <array01.length ; i++) {
+            System.out.print(array01[i]+" ");
+        }
+
+        System.out.print("\n");
+        System.out.print("Enter the size of array: ");
+        int size = keyboard.nextInt();
+        int[] array1 = new int[size];
+ 
+        int value=2;
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = value;
+            value+=2;
+        }
+        for (int i = 0; i <array1.length ; i++) {
+            System.out.print(array1[i]+" ");
+        }
+        System.out.print("\n");
+    }
+}
